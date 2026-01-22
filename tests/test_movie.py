8248509c57a1c30aeca617a4225af4e55323bef5
noemioliver <noemioliverborja.com>
2026-01-22 11:51:01 +0100

    o

---
+++ tests/test_movie.py
@@ -0,0 +1,17 @@
+def test_create():
+    pass
+
+def test_update():
+    pass
+
+def test_delete():
+    pass
+
+def test_obtener_pelicula_no_encontrada():
+    pass
+
+def test_actualizar_pelicula_inexistente():
+    pass
+
+def test_eliminar_pelicula_existente():
+    pass
